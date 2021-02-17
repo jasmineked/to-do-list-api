e1783cc73a97eb00441b1f665fe4d5eb1e95783d
jasmineked <jked630@icloud.com>
2021-02-17 13:49:32 -0500

    remove property from userschema: testing

--- app/models/user.js
+++ app/models/user.js
@@ -10,10 +10,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true
   },
-  username: {
-    required: true,
-    unique: true
-  },
+  // username: {
+  //   required: false,
+  //   unique: true
+  // },
   token: String
 }, {
   timestamps: true,
